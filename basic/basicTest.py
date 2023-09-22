1c52a58e5e2b198dfd6ab05c1be41caebb8a35da
avilagtz <a52xmax@gmail.com>
2023-09-21 23:33:31 -0600

    Code documented and refactor 2

--- basic/basicTest.py
+++ basic/basicTest.py
@@ -5,28 +5,34 @@ from time import sleep
 # URL
 automotiveSpeedometerUrl = "file:///C:/Users/avilagtz/Documents/Coding/Python/Automation_course/basic/carSpeedometer/dist/index.html"
 
-# XPATHS
+# XPATHS are the location of the elements in the UI
 speedInputXpath = "/html/body/div[2]/input"
 speedInputSubmitXpath = "/html/body/div[2]/button"
 
 digitalSpeedXpath = "/html/body/div[1]/div/div[64]/div[1]"
 gaugeSpeedAngleXpath = "/html/body/div[1]/div/div[62]"
 
+# Launch the chrome browser and open the UI
 driver = webdriver.Chrome()
 driver.get(automotiveSpeedometerUrl)
 sleep(10)
 
+# Find the speed input in the UI
 speedInput = driver.find_element("xpath", speedInputXpath)
 speedInput.send_keys("100")
 
+# Find the speed send button in the UI
 speedInputSubmit = driver.find_element("xpath", speedInputSubmitXpath)
 speedInputSubmit.click()
 
+# Wait for the UI to update the requested value
 sleep(5)
 
+# Find the digital speed in the UI and get the value
 digitalSpeed = driver.find_element("xpath", digitalSpeedXpath)
 print("Digital speed:", digitalSpeed.text)
 
+# Find the gauge angle and get the value
 gaugeSpeedDeg = driver.find_element("xpath", gaugeSpeedAngleXpath)
 print("Gauge angle:", gaugeSpeedDeg.get_attribute('style'))
 
