2bac76fac6089af563dac0dded6ab79094fbdf9f
avilagtz <a52xmax@gmail.com>
2023-09-21 23:30:16 -0600

    Code documented and refactor

--- full/reportGenerator.py
+++ full/reportGenerator.py
@@ -3,17 +3,10 @@ from openpyxl.styles import Font, Color
 from openpyxl.styles import PatternFill
 from openpyxl.utils.dataframe import dataframe_to_rows
 
-# Datos
-data = [
-    {"testName": "test_initial_speed", "speed": "0", "expectedDeg": "-120deg", "result": "FAIL"},
-    {"testName": "test_30_speed", "speed": "30", "expectedDeg": "-105deg", "result": "FAIL"},
-    {"testName": "test_60_speed", "speed": "60", "expectedDeg": "-90deg", "result": "PASS"},
-    {"testName": "test_90_speed", "speed": "90", "expectedDeg": "-75deg", "result": "FAIL"},
-    {"testName": "test_120_speed", "speed": "120", "expectedDeg": "-60deg", "result": "FAIL"},
-    {"testName": "test_130_speed", "speed": "500", "expectedDeg": "-130deg", "result": "FAIL"},
-]
-
+# Define the initial row to be written
 initWritableRow = 4
+
+# Define the location of the columns
 testNameCol = "C"
 speedToTestCol = "D"
 expectedDegCol = "E"
@@ -25,36 +18,42 @@ def generateReport(data, reportName):
     testResultsSheet = excelWorkbook["TestsResults"]
     stylesSheet = excelWorkbook["Styles"]
 
+    # Get the styles to be used
     yellowStyle = stylesSheet["A1"]
     redStyle = stylesSheet["A2"]
     greenStyle = stylesSheet["A3"]
     emptyStyle = stylesSheet["A4"]
 
     for test in data:
+        # Create the coordinates of the columns to be written
         testNameColWritable = testNameCol + str(writableRow)
         speedToTestColWritable = speedToTestCol + str(writableRow)
         expectedDegColWritable = expectedDegCol + str(writableRow)
         resultColWritable = resultCol + str(writableRow)
 
+        # Apply the style to the coordinate to be written and then add the required information.
         testResultsSheet[testNameColWritable]._style = emptyStyle._style
         testResultsSheet[testNameColWritable].value = test["testName"]
 
+        # Apply the style to the coordinate to be written and then add the required information.
         testResultsSheet[speedToTestColWritable]._style = emptyStyle._style
         testResultsSheet[speedToTestColWritable].value = test["speed"]
 
+        # Apply the style to the coordinate to be written and then add the required information.
         testResultsSheet[expectedDegColWritable]._style = emptyStyle._style
         testResultsSheet[expectedDegColWritable].value = test["expectedDeg"]
 
+        # Apply the style to the coordinate based on the test result then add the required information.
         if test["result"] == "PASS":
             testResultsSheet[resultColWritable]._style = greenStyle._style
         else:
             testResultsSheet[resultColWritable]._style = redStyle._style
         testResultsSheet[resultColWritable].value = test["result"]
 
+        # Increase the row to write
         writableRow += 1
 
-
+    #Save the report
     excelWorkbook.save(reportName + ".xlsx")
     return True
 
-generateReport(data,"testReport")
--- full/test_execution.py
+++ full/test_execution.py
@@ -19,9 +19,10 @@ from reportGenerator import *
 # Crete an instance of selenium web driver and open the 'automotiveSpeedometerUrl'
 driver = webdriver.Chrome()
 driver.get(automotiveSpeedometerUrl)
-sleep(2)
+sleep(10)
 
 def getDegFromGaugeStyle(gaugeStyle):
+    # TO RESEARCH: REGULAR EXPRESSIONS
     #'transform: rotate(-70deg); transition: none 0s ease 0s;'
     pattern = r'-?\d+deg'
     deg = re.findall(pattern, gaugeStyle)
@@ -36,18 +37,21 @@ def setSpeed(speed):
     speedInputSubmit.click()
     sleep(5)
 
-
+# This function will get the test cases from the json file
 def loadTestsFromJson(jsonFile):
     with open(jsonFile, 'r') as archivo:
         testCases = json.load(archivo)
     return testCases
 
-
+# Load the json file and store it in the variable 'testCases'
 testCases = loadTestsFromJson("testCases.json")
 
 
+# TO RESEARCH: PYTEST MARKERS
+# This marker receives the 'testCases' defined in the jsonfile
 @pytest.mark.parametrize("testCase", testCases)
 def test_all(testCase):
+    # Each test case contains the keys 'testName', 'speed', 'expectedDeg' and 'result'
     testName = testCase["testName"]
     speedToSet = testCase["speed"]
     expectedDeg = testCase["expectedDeg"]
@@ -55,6 +59,7 @@ def test_all(testCase):
     print("Speed to set:", speedToSet)
     print("Expected deg:", expectedDeg)
 
+    # Set the speed defined by the test case
     setSpeed(speedToSet)
 
     digitalSpeed = driver.find_element("xpath", digitalSpeedXpath)
@@ -68,10 +73,12 @@ def test_all(testCase):
     assert expectedDeg == initialGaugeDeg[0]
     print("Gauge deg correct")
 
+    # As no assetion failed, then change the test result to PASS
     testCase["result"] = "PASS"
 
     setSpeed("0")
 
+# This test will generate the report
 def test_print_data():
     reportStatus = generateReport(testCases, reportName)
     assert reportStatus == True
